4f6ecc92a60e64c9ce9d04b5f0a4e698b3735bde
Anis <chaabani.anis@gmail.com>
2019-05-20 14:10:19 +0200

    add test throw exception when no logged user

--- TripServiceKata.Tests/TripServiceTest.cs
+++ TripServiceKata.Tests/TripServiceTest.cs
@@ -1,10 +1,32 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripServiceKata.Exception;
+using TripServiceKata.Trip;
 
 namespace TripServiceKata.Tests
 {
     [TestClass]
     public class TripServiceTest
     {
-        
+        [TestMethod]
+        [ExpectedException(typeof(UserNotLoggedInException))]
+        public void Should_Throw_Exception_When_No_Logged_User()
+        {
+            //Arrange
+            User.User unloggedUser = null;
+            TripService tripService = new TestTripService();
+
+            //Act
+            tripService.GetTripsByUser(unloggedUser);
+
+            //Assert exception
+        }
+    }
+
+    public class TestTripService : TripService
+    {
+        public override User.User GetLoggedUser()
+        {
+            return null;
+        }
     }
 }
