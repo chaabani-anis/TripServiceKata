310bc0aa065f44239c0038ea596e7fb13892d2d2
Anis <chaabani.anis@gmail.com>
2019-05-24 11:54:59 +0200

    Refacto

--- TripServiceKata.Tests/TripServiceTest.cs
+++ TripServiceKata.Tests/TripServiceTest.cs
@@ -1,10 +1,95 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TripServiceKata.Exception;
+using TripServiceKata.Trip;
 
 namespace TripServiceKata.Tests
 {
     [TestClass]
     public class TripServiceTest
     {
-        
+        private TripService _tripService;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _tripService = new TripService();
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(UserNotLoggedInException))]
+        public void Should_Return_Exception_When_User_Not_Logged()
+        {
+            //Arrange
+            User.User user = null;
+            
+            //Act
+            _tripService.GetTripsByUser(user, null);
+        }
+
+        [TestMethod]
+        public void Should_Return_No_Trips_When_User_Has_No_Friends()
+        {
+            //Arrange
+            var user1 = new User.User();
+            var loggedUser = new User.User();
+
+            //Act
+            var listTrips = _tripService.GetTripsByUser(user1, loggedUser);
+
+            //Assert
+            Assert.IsFalse(listTrips.Any());
+        }
+
+        [TestMethod]
+        public void Should_Return_No_Trip_When_Users_Are_Not_Friends()
+        {
+            //Arrange
+            var user = new User.User();
+            user.AddFriend(new User.User());
+            var loggedUser = new User.User();
+            loggedUser.AddFriend(new User.User());
+
+            //Act
+            var listTrips = _tripService.GetTripsByUser(user, loggedUser);
+
+            //Assert
+            Assert.IsFalse(listTrips.Any());
+        }
+
+        [TestMethod]
+        public void Should_Return_Trips_When_Users_Are_Friends()
+        {
+            //Arrange
+            _tripService = new TestableTripService();
+            var user = new User.User();
+            
+            var friend = new User.User();
+            user.AddFriend(friend);
+
+            var brazil = new Trip.Trip();
+            user.AddTrip(brazil);
+
+            //Act
+            var listTrips = _tripService.GetTripsByUser(user, friend);
+
+            //Assert
+            Assert.IsTrue(listTrips.Any());
+        }
+
+        private class TestableTripService : TripService
+        {
+            protected override User.User LoggedUsers()
+            {
+                return null;
+            }
+
+            protected override List<Trip.Trip> GetTripsBy(User.User user)
+            {
+                return user.Trips();
+            }
+        }
     }
 }
--- TripServiceKata/Trip/TripService.cs
+++ TripServiceKata/Trip/TripService.cs
@@ -6,31 +6,52 @@ namespace TripServiceKata.Trip
 {
     public class TripService
     {
-        public List<Trip> GetTripsByUser(User.User user)
+        public List<Trip> GetTripsByUser(User.User user, User.User loggedUser)
         {
             List<Trip> tripList = new List<Trip>();
-            User.User loggedUser = UserSession.GetInstance().GetLoggedUser();
             bool isFriend = false;
-            if (loggedUser != null)
-            {
-                foreach(User.User friend in user.GetFriends())
-                {
-                    if (friend.Equals(loggedUser))
-                    {
-                        isFriend = true;
-                        break;
-                    }
-                }
-                if (isFriend)
-                {
-                    tripList = TripDAO.FindTripsByUser(user);
-                }
-                return tripList;
-            }
-            else
+            if (loggedUser == null)
             {
                 throw new UserNotLoggedInException();
             }
+            else
+            {
+                isFriend = AreUserFriends(user, loggedUser);
+
+                if (isFriend)
+                {
+                    tripList = GetTripsBy(user);
+                }
+
+                return tripList;
+            }
+        }
+
+        private bool AreUserFriends(User.User user, User.User loggedUser)
+        {
+            bool isFriend = false;
+            foreach (User.User friend in user.GetFriends())
+            {
+                if (friend.Equals(loggedUser))
+                {
+                    isFriend = true;
+                    break;
+                }
+            }
+            return isFriend;
+        }
+
+        protected virtual List<Trip> GetTripsBy(User.User user)
+        {
+            List<Trip> tripList;
+            tripList = TripDAO.FindTripsByUser(user);
+            return tripList;
+        }
+
+        protected virtual User.User LoggedUsers()
+        {
+            User.User loggedUser = UserSession.GetInstance().GetLoggedUser();
+            return loggedUser;
         }
     }
 }
